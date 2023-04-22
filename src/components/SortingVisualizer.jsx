d0f8c40dc663d990f401fc02c0bfdcc275021690
Zahid Safi <zahidsafi956@gmail.com>
2023-04-22 15:03:30 -0600

    added package-lock.json

--- src/components/SortingVisualizer.jsx
+++ src/components/SortingVisualizer.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Button } from 'antd';
 import './SortingVisualizer.css'
+
 
 export default class SortingVisualizer extends React.Component {
     constructor(props) {
@@ -16,24 +18,44 @@ export default class SortingVisualizer extends React.Component {
 
     resetArray() {
         const array = [];
-        for (let i = 0; i < 310; i++) {
-            array.push(randomIntFromInterval(5, 730));
+        for (let i = 0; i < 1000; i++) {
+            array.push(randomIntFromInterval(5, 1000));
         }
         this.setState({ array });
     }
 
- 
+
 
     render() {
         const { array } = this.state;
 
         return (
-            <div className="array-container">
-                {array.map((value, idx) => (
-                    <div className="array-bar" key={idx} style={{height: `${value}px`}}>
+            <>
+                <header>
+                    <button onClick={ () => this.resetArray() }>
+                        Generate New Array
+                    </button>
+                    <div>
+
+                        <select>
+
+                            <option value="fruit">Selection Sort</option>
+
+                            <option value="insertionsort">Insertion Sort</option>
+
+                            <option value="quicksort">Quick Sort</option>
+
+                        </select>
+
                     </div>
-                ))}
-            </div> 
+                </header>
+                <div className="array-container">
+                    {array.map((value, idx) => (
+                        <div className="array-bar" key={idx} style={{ height: `${value}px` }}>
+                        </div>
+                    ))}
+                </div>
+            </>
         )
     }
 }
