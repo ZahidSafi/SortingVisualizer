737122ea4b6d977f96f44ff744f525be7dfbf816
Zahid Safi <zahidsafi956@gmail.com>
2023-04-23 22:14:30 -0600

    added a slider

--- src/components/SortingVisualizer.jsx
+++ src/components/SortingVisualizer.jsx
@@ -10,6 +10,7 @@ export default class SortingVisualizer extends React.Component {
 
         this.state = {
             array: [],
+            timeout: 0,
         };
     }
 
@@ -25,48 +26,70 @@ export default class SortingVisualizer extends React.Component {
         this.setState({ array });
     }
 
-    async animateSort(animations) {
+    swap(arrayBars, animation) {
+        const tempHeight = arrayBars[animation.index1].style.height;
+        arrayBars[animation.index1].style.height = arrayBars[animation.index2].style.height;
+        arrayBars[animation.index2].style.height = tempHeight
+    }
+
+    compare(arrayBars, animation) {
+        arrayBars[animation.index1].style.backgroundColor = 'red';
+        arrayBars[animation.index2].style.backgroundColor = 'red';
+    }
+
+    async animateBubbleSort(animations) {
         const arrayBars = document.getElementsByClassName('array-bar');
         for (let i = 0; i < animations.length; i++) {
             let animation = animations[i];
-            if (animation.type === 'current') {
-                arrayBars[animation.index1].style.backgroundColor = 'green';
-            }
             if (animation.type === 'compare') {
-                arrayBars[animation.index1].style.backgroundColor = 'red';
-                arrayBars[animation.index2].style.backgroundColor = 'red';
+                this.compare(arrayBars, animation);
             }
             if (animation.type === 'swap') {
-                const tempHeight = arrayBars[animation.index1].style.height;
-                arrayBars[animation.index1].style.height = arrayBars[animation.index2].style.height;
-                arrayBars[animation.index2].style.height = tempHeight
+                this.swap(arrayBars, animation)
+            }
+            await new Promise((resolve) => setTimeout(resolve, this.state.timeout));
+            arrayBars[animation.index1].style.backgroundColor = 'white';
+            arrayBars[animation.index2].style.backgroundColor = 'white';
+
+        }
+    }
+
+    async animateInsertionSort(animations) {
+        const arrayBars = document.getElementsByClassName('array-bar');
+        for (let i = 0; i < animations.length; i++) {
+            let animation = animations[i];
+            if (animation.type === 'compare') {
+                this.compare(arrayBars, animation);
+            }
+            if (animation.type === 'swap') {
+                this.swap(arrayBars, animation)
             }
             if (animation.type === 'overwrite') {
                 arrayBars[animation.index1].style.height = `${animation.value}px`;
             }
-            await new Promise((resolve) => setTimeout(resolve, 0));
+            await new Promise((resolve) => setTimeout(resolve, this.state.timeout));
 
             arrayBars[animation.index1].style.backgroundColor = 'white';
 
-
-            if (animation.type === 'compare' || animation.type === 'swap') {
+            if (animation.type === 'compare') {
                 arrayBars[animation.index2].style.backgroundColor = 'white';
             }
-
 
         }
     }
 
     handleSort(event) {
         const value = event.target.value;
-        let animations = [];
         if (value === 'bubble') {
-            animations = bubbleSort(this.state.array);
+            this.animateBubbleSort(bubbleSort(this.state.array));
         }
         else if (value === 'insertion') {
-            animations = insertionSort(this.state.array);
+            this.animateInsertionSort(insertionSort(this.state.array));
         }
-        this.animateSort(animations);
+    }
+
+    changeTimeout(event) {
+        this.setState({ timeout: event.target.value });
     }
 
 
@@ -91,6 +114,7 @@ export default class SortingVisualizer extends React.Component {
                         <option value="bubble">Bubble Sort</option>
                         <option value="insertion">Insertion Sort</option>
                     </select>
+                    <input type="range" min="0" max="100" value={this.state.timeout} onChange={(event) => this.changeTimeout(event)} />
                 </header>
                 <div className="array-container">
                     {array.map((value, idx) => (
